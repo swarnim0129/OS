f8ae24f05c30351e495ae2c4e0183d59e16b66d8
Swarnim Bane <136164833+swarnim0129@users.noreply.github.com>
2025-05-10 03:54:12 +0530

    fits

--- first_best_worstFit.py
+++ first_best_worstFit.py
@@ -2,41 +2,35 @@ list = []
 n = 4
 for i in range(n):
     list.append(int(input("Enter adddress: ")))
-segment = {100: 0, 500: 0, 200: 0, 300: 0, 600: 0}
-#first fit
+segment1 = {100: 0, 500: 0, 200: 0, 300: 0, 600: 0}
+
 for i in range(len(list)):
-    for key in segment:
-        if segment[key] == 0 and key >= list[i]:
-            segment[key] = list[i]
-            break 
-print('first fit',segment)
+    for key in segment1:
+        if segment1[key]==0 and list[i]<=key:
+            segment1[key]=list[i]
+            break
 
-#best fit
+print(segment1)
+
 segment = {100: 0, 500: 0, 200: 0, 300: 0, 600: 0}
-for req in list:
-    best_key = None
-    best_size = float('inf')  
-
+for i in range(len(list)):
+    best_key=float("inf")
     for key in segment:
-        if segment[key] == 0 and key >= req:
-            if key < best_size:
-                best_size = key
-                best_key = key
+        if segment[key]==0 and key>=list[i]:
+            if key<best_key:
+                best_key=key
+    segment[best_key]=list[i]
+print(segment)
 
-    if best_key is not None:
-        segment[best_key] = req
-print('best fit',segment)
 
-#worst fit
-segment = {100: 0, 500: 0, 200: 0, 300: 0, 600: 0}
-for req in list:
-    worst_key = None
-    worst_size = -1
-    for key in segment:
-        if segment[key] == 0 and key >= req:
-            if key > worst_size:
-                worst_size = key
-                worst_key = key200
-    if worst_key is not None:
-        segment[worst_key] = req
-print('Worst Fit:', segment)
+segment3 ={100: 0, 500: 0, 200: 0, 300: 0, 600: 0}
+
+for i in range(len(list)):
+    worst_key=-1
+    for key in segment3:
+        if segment3[key]==0 and key>=list[i]:
+            if key>worst_key:
+                worst_key=key
+    if worst_key!=-1:
+        segment3[worst_key]=list[i]
+print(segment3)
